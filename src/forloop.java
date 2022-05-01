1b92a826346486201bd63c2e82a2ebb68b15cf12
ISHmakhija <raj372721@gmail.com>
2022-05-01 20:37:59 +0100

    src/forloop.java

---
+++ src/forloop.java
@@ -0,0 +1,16 @@
+public class forloop
+{
+    public static void main(String[] args)
+    {
+        int a =2;
+        for (int i = 0; i <= 12 ; i++)
+        {
+            System.out.println(a+" x "+i+" = "+(i*a));
+        }
+
+
+
+
+
+    }
+}
