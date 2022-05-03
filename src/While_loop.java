daf729b99bcc0cfe1c9d3ea58485a46c7df36157
ISHmakhija <raj372721@gmail.com>
2022-05-03 20:14:31 +0100

    main

---
+++ src/While_loop.java
@@ -0,0 +1,15 @@
+public class While_loop
+{
+    public static void main(String[] args) {
+
+
+        int a=2;
+        int b=1;
+
+        while (b<=100) {
+            System.out.println(a+" x "+b+" = "+(a*b));
+            b++;
+        }
+    }
+
+}
